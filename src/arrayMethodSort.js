b402b55c1fa81b28b8427819baabeb93592bfc6f
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-20 01:14:07 +0200

    Array method sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,14 @@
-'use strict';
-
-/**
- * Implement method Sort
- */
-function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+Array.prototype.sortCustom = function(compareFn) {
+  const defaultCompare = (a, b) => {
+    const strA = String(a);
+    const strB = String(b);
+    return strA < strB ? -1 : strA > strB ? 1 : 0;
   };
-}
+
+  const compare = typeof compareFn === 'function' ? compareFn : defaultCompare;
+
+  // Wywołanie sort2 w kontekście bieżącej tablicy
+  return [].__proto__.sort2.call(this, compare);
+};
 
 module.exports = applyCustomSort;
